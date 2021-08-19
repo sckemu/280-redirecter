71079866a535d05d1fffc69d7f43103b4e9e1c2d
sckemu <you@example.com>
2021-08-19 13:39:19 +0900

    Add PC settings

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ require("date-utils");
 var dt = new Date();
 
 var server = app.listen(8080,function(){
-    console.log("Listen Port:" + server.address().port);
+    console.log("A Listen Port:" + server.address().port);
 });
 
 app.get("/", function(req,res){
     res.redirect(302,'https://280blocker.net/files/280blocker_adblock_' + dt.toFormat("YYYYMM") + '.txt')
+});
+app.get("/pc", function(req,res){
+    res.redirect(302,'https://raw.githubusercontent.com/Yuki2718/adblock/master/japanese/jp-filters.txt')
 });
