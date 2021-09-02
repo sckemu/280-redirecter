570b8fe9745135462942da6235ad2c8ce51ce857
sckemu <49110580+sckemu@users.noreply.github.com>
2021-09-02 15:48:56 +0900

    change to yuki-mob

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var app = express();
 require("date-utils");
-var dt = new Date();
 
 var server = app.listen(8080,function(){
     console.log("A Listen Port:" + server.address().port);
 });
 
-app.get("/", function(req,res){
-    res.redirect(302,'https://280blocker.net/files/280blocker_adblock_' + dt.toFormat("YYYYMM") + '.txt')
+app.get("/sp", function(req,res){
+    res.redirect(302,'https://raw.githubusercontent.com/Yuki2718/adblock/master/japanese/jp-mob.txt')
 });
 app.get("/pc", function(req,res){
     res.redirect(302,'https://raw.githubusercontent.com/Yuki2718/adblock/master/japanese/jp-filters.txt')
